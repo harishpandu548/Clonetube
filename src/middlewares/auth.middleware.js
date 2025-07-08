5d9781109cd99fe5a8b7f1e03c12e5d2f7096c5c
harishpandu548 <harishpandu548@gmail.com>
2025-07-09 00:37:26 +0530

    Complete backend code

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,31 +1,28 @@
-import Apierrors from "../utils/Apierrors.js";
+// import Apierrors from "../utils/Apierrors.js";
 import { User } from "../models/user.models.js";
 import jwt from "jsonwebtoken";
-
 
 export const verifyJWT = async (req, res, next) => {
   try {
     const token = req.cookies?.accessToken;
 
-    if (! token) {
-      throw new Apierrors(401, "you are not logged in");
+    if (!token) {
+      return res.status(401).json({ success: false, message: "No access token. You are not logged in." });
     }
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-    const user = await User.findById(decodedToken?._id).select(
-      "-password -refreshToken",
-    );
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
 
     if (!user) {
-      throw new Apierrors(401, "you are not logged in");
+      return res.status(401).json({ success: false, message: "User not found. You are not logged in." });
     }
 
-    req.user = user;
+    req.user = user; 
     next();
 
   } catch (error) {
-    console.log("error at logout middleware",error.message)
-    throw new Apierrors(401, "you are not logged in");
+    console.log("error at verifyJWT middleware", error.message);
+    return res.status(401).json({ success: false, message: "Invalid token or login expired" });
   }
 };
