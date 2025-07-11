562fa06e5d068243c8b592073fef64af71b075cc
harishpandu548 <harishpandu548@gmail.com>
2025-07-12 03:03:18 +0530

    changed some portion of backend

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import dotenv from "dotenv";
 import dbConnection from "./db/index.js";
 import { app } from "./app.js"; //  Use this one!
 
+import path from "path";
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "client/dist"))); // or build folder
+
+// ⬇️ Catch-all route for frontend routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
+});
+
+
 // this step was important bcz when app runs the first file running is index.js as we made in package.josn as index.js as entry file so when this loads we try to load env varaibles as soon as possible to get available for others
 dotenv.config({
     path:"./.env"
